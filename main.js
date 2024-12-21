c8e62831ac8b7499db5d20f8f41d7f040725ebf1
Tolani <omotolaniafolayan85@gmail.com>
2024-12-21 21:11:04 +0100

    cgpa classification commits

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 function showResult() {
     var userCgpa = Number(document.getElementById('cgpa').value);
     // var userCgpa = Number(cgpa.value);
+    var songfail = new Audio("audio/fail.mp3")
+    var songpass = new Audio("audio/fail.mp3")
+    var songthird = new Audio("audio/third.wav")
+    var song2lower = new Audio("audio/lower.mp3")
+    var song2upper = new Audio("audio/upper.wav")
+    var songfirst = new Audio("audio/firstclass.wav")
 
-    if(userCgpa > 5) {
-        document.getElementById("classification").innerHTML = `Invalid. Please enter a CGPA between 0 and 5.`
-        // classification.value = 'Invalid. Please enter a CGPA between 0 and 5.'
+    if (isNaN(userCgpa) || userCgpa < 0 || userCgpa > 5) {
+        document.getElementById("classification").innerHTML = `Invalid. Please enter a CGPA between 0 and 5.`;
+        return;
+    }
+    
+    
+    if (userCgpa <= 0.99) {
+        document.getElementById("classification").innerHTML = `Probation &#128078;. Advise to withdraw`;
+        songfail.play();
+    } else if(userCgpa <= 1.49) {
+        document.getElementById("classification").innerHTML = `Pass. You can do better`;
+        songpass.play();
+    } else if(userCgpa <= 2.39) {
+        document.getElementById("classification").innerHTML = `Third Class`;
+        songthird.play();
+    } else if (userCgpa <= 3.49) {
+        document.getElementById("classification").innerHTML = `Second Class Lower.`;
+        song2lower.play();
+    } else if (userCgpa <= 4.49) {
+        document.getElementById("classification").innerHTML = `Second Class Upper.`;
+        song2upper.play();
+    } else if (userCgpa <= 5.0) {
+        document.getElementById("classification").innerHTML = `Congratulations &#127881;. You got first class.`;
+        songfirst.play();
     }
 }
