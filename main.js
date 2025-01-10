102a6e90e43a1beffe65e3648d5d5c6e43696efa
Tolani <omotolaniafolayan85@gmail.com>
2025-01-10 20:49:27 +0100

    letter_grade updated

--- main.js
+++ main.js
@@ -7,10 +7,6 @@ function showResult() {
     var song2lower = new Audio("audio/lower.mp3")
     var song2upper = new Audio("audio/upper.wav")
     var songfirst = new Audio("audio/firstclass.wav")
-
-    if (isNaN(userCgpa) || userCgpa < 0 || userCgpa > 5) {
-        document.getElementById("classification").innerHTML = `Invalid. Please enter a CGPA between 0 and 5.`;
-    }
      
     if (userCgpa <= 0.99) {
         document.getElementById("classification").innerHTML = `Probation &#128078;. Advise to withdraw`;
@@ -30,20 +26,29 @@ function showResult() {
     } else if (userCgpa <= 5.0) {
         document.getElementById("classification").innerHTML = `Congratulations &#127881;. You got first class.`;
         songfirst.play();
+    }else {
+        document.getElementById("classification").innerHTML = `Invalid. Input a number between 0 and 5.0`
     }
 }
 
 
 
 function showLgrade(){
-    var userGrade = Number(document.getElementById('l_grade')).value;
+    var userGrade = Number(document.getElementById('l_grade').value);
 
-    if(isNaN(userGrade) || userGrade < 0 || userGrade > 100){
-        document.getElementById("letter_result").innerHTML = `Invalid. Please enter a number between 0 and 100.`;
+    if (userGrade >= 70 && userGrade <= 100) {
+        document.getElementById("letter_result").innerHTML = `A`;
+    } else if (userGrade >= 60 && userGrade < 70) {
+        document.getElementById("letter_result").innerHTML = `B`;
+    } else if (userGrade >= 50 && userGrade < 60) {
+        document.getElementById("letter_result").innerHTML = `C`;
+    } else if (userGrade >= 40 && userGrade < 50) {
+        document.getElementById("letter_result").innerHTML = `D`;
+    } else if (userGrade >= 30 && userGrade < 40) {
+        document.getElementById("letter_result").innerHTML = `E`;
+    } else if (userGrade >=0 && userGrade < 30) {
+        document.getElementById("letter_result").innerHTML = `F`;
+    } else {
+        document.getElementById("letter_result").innerHTML = `Invalid. Kindly input a number between 0 and 100`
     }
-
-    if (userGrade <= ) {
-        
-    }
-    
 }
